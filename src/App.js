141bef0a1b710c11ec1e30b9b33c2bac806f4531
Tezaen <Philippines420AD>
2021-01-18 14:39:56 -0800

    added another component, UserItem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import React, { Component } from 'react';
 
 class App extends Component{
@@ -7,6 +8,7 @@ class App extends Component{
     return (
       <div className='App'>
         <Navbar title={'Roober\'s Github Finder'}/>
+        <UserItem />
       </div>
     );
   }
